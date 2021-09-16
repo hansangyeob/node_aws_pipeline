66d060ef30a0839a97041f09bf8467dff637c36f
Han sang yeob <s3821179@rmit.edu.vn>
2021-09-17 01:48:28 +0700

    added function

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the homepage");
+    const a = 1
+    const b = 2
+    res.send(a+b);
+        res.send('yo');
 });
 
+app.get('/add', function(req, res) {
+    var a = Number(req.query.a);
+    var b = Number(req.query.b)
+    if (!isNaN(a) && !isNaN(b)) {
+        res.send((a + b).toString())
+    } else {
+        res.send("a, b must be number")
+    }
+});
 
 
 const port = process.env.port || 3000;
