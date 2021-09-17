a7054a1f14ca3ad4abb179229bf3173454910a26
Tony Han <57244611+hansangyeob@users.noreply.github.com>
2021-09-17 10:26:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,14 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the homepage");
-    const a = 1
-    const b = 2
-    res.send(a+b);
-        res.send('yo');
 });
 
-app.get('/add', function(req, res) {
-    var a = Number(req.query.a);
-    var b = Number(req.query.b)
-    if (!isNaN(a) && !isNaN(b)) {
-        res.send((a + b).toString())
-    } else {
-        res.send("a, b must be number")
-    }
-});
+
 
 app.get('/process',function(req,res){
-    var myStr = req.query
-    var input = myStr.replace(/  +/g, ' ').trim();
-    res.send(input.toString());
+    var input = req.query.myStr1;
+    res.send(input);
 });
-
 
 const port = process.env.port || 3000;
 app.listen(port,()=> {
